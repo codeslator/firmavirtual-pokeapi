e0e7ceb9c14b57adae6ed4a64c5be466a323ce3a
Andrés Meléndez <codeslator@gmail.com>
2024-06-04 10:56:27 -0400

    prettify code;

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-  .setTitle('PokemonAPI FirmaVirtual API')
-  .setDescription('This API connects with PokeAPI to get data and store to the own database. Remember execute seeder to download data.')
-  .setVersion('1.0')
-  .addTag('pokemon')
-  .build();
-const document = SwaggerModule.createDocument(app, config);
-SwaggerModule.setup('api', app, document);
+    .setTitle('PokemonAPI FirmaVirtual API')
+    .setDescription('This API connects with PokeAPI to get data and store to the own database. Remember execute seeder to download data.')
+    .setVersion('1.0')
+    .addTag('pokemon')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
 }
